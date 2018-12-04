0a1d9f7a896cd182bb4016f1ad0364fa093c72d9
MatthewKligerman <mkligerman@fordham.edu>
2018-12-03 19:47:18 -0500

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -11,13 +11,20 @@ with open("adult.train.csv") as csvfile:
         else:
             headers = row
 
+print('Headers : ')
 print(headers)
-print(trainData[1:3])
-#print(results)
-
+print('\n')
+print('Training Data : ')
+trainData = np.asarray(trainData)
+print(trainData)
+print('\n')
+print('Testing Data : ')
 testData = []
 with open("adult.test.csv") as csvfile:
     reader = csv.reader(csvfile) # change contents to floats
     for i, row in enumerate(reader): # each row is a list
         if i!=0:
             testData.append(row)
+
+testData = np.asarray(testData)
+print(testData)
