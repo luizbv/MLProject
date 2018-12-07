6dbb5a3d11e2490bdba13afe04b375972cbd2c5b
MatthewKligerman <mkligerman@fordham.edu>
2018-12-07 10:56:40 -0500

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -2,7 +2,7 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 
-def getLikelikehood(train, header, yValue):
+def getLikelihood(train, header, yValue):
     return 1
 
 def probability50k(train, header, label, yValue):
@@ -10,26 +10,27 @@ def probability50k(train, header, label, yValue):
     falseSum = 0
 
     for item in train.T[train.T[0]==header]==label:
-        
+
         if item == '>50K':
             trueSum += 1
+
         else:
             falseSum += 1
-            
+
     return trueSum/(trueSum+falseSum)
 
 headers = []
 trainData = []
 
 with open("adult.train.csv") as csvfile:
-    
+
     reader = csv.reader(csvfile) # change contents to floats
-    
+
     for i, row in enumerate(reader): # each row is a list
-        
+
         if i!=0:
             trainData.append(row)
-            
+
         else:
             headers = row
 
@@ -44,11 +45,11 @@ print(trainData)
 testData = []
 
 with open("adult.test.csv") as csvfile:
-    
+
     reader = csv.reader(csvfile) # change contents to floats
-    
+
     for i, row in enumerate(reader): # each row is a list
-        
+
         if i!=0:
             testData.append(row)
 
@@ -60,18 +61,17 @@ print(testData)
 print('\n\n')
 # Average for each feature independently: workclass, education, marital-status, occupation, relationship, race, sex,
 # native-country
-avgList = ['workclass', 'education', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'native-country', 'earnings'][]
+avgList = ['workclass', 'education', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'native-country', 'earnings']
 avgs =  {}
 
 # creates dictionary, each containing the header as a key with the value as an array of possible characteristics
 for i, item in enumerate(trainData.T):
-    
+
     if headers[i] in avgList:
-        
         avgs[headers[i]] = []
-        
+
         for info in item:
-            
+
             if info not in avgs[headers[i]]:
                 avgs[headers[i]].append(info)
 
@@ -82,15 +82,13 @@ probs = {}
 #for i, key in enumerate(avgs.keys()):
 for item in trainData.T:
 
-    if item in avgs.keys():
-        
-        probs[item] = []
-        
-        for key in avgs.keys()[avgs.keys()==item]:
-            
-            probs[item].append(probability50k(trainData, item, key, len(trainData.T))})
+    if item[0] in avgs.keys():
+
+        probs[item[0]] = []
+
+        for key in avgs.keys()[avgs.keys()==item[0]]:
+            probs[item[0]].append(probability50k(trainData, item[0], key, len(trainData.T)))
 
     else:
-        
-        probs[item] = []
-        probs[item].append(getLikelihood(trainData, item, len(trainData)))
+        probs[item[0]] = []
+        probs[item[0]].append(getLikelihood(trainData, item[0], len(trainData)))
