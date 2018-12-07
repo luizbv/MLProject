f40c4a39c4ccb929e4eb39d76411d8655273a6f5
luizbv <lbenitez1@fordham.edu>
2018-12-07 14:33:55 -0500

    Update SVM.py

--- SVM.py
+++ SVM.py
@@ -2,22 +2,23 @@ import csv
 import numpy as np
 import matplotlib.pyplot as plt
 
-def getLikelihood(train, header, yValue):
+def getLikelihood(train, header):
     return 1
 
-def probability50k(train, header, label, yValue):
+def probability50k(train, header, label):
     trueSum = 0
     falseSum = 0
 
-    for item in train.T[train.T[0]==header]==label:
+    for i, item in enumerate(train[:].T):
+        if item[0] == header:
+            for e, val in enumerate(item[1:]):
+                if val == label:
+                    if '>50K' in train.T[len(train[:][0])-1][e+1]:
+                        trueSum += 1
+                    else:
+                        falseSum += 1
 
-        if item == '>50K':
-            trueSum += 1
-
-        else:
-            falseSum += 1
-
-    return trueSum/(trueSum+falseSum)
+    return {label:(trueSum/(trueSum+falseSum), trueSum, falseSum)}
 
 headers = []
 trainData = []
@@ -28,11 +29,11 @@ with open("adult.train.csv") as csvfile:
 
     for i, row in enumerate(reader): # each row is a list
 
-        if i!=0:
-            trainData.append(row)
-
-        else:
+        if i == 0:
             headers = row
+        if ' ?' not in row:
+            trainData.append(row)
+            
 
 print('Headers : ')
 print(headers)
@@ -50,8 +51,11 @@ with open("adult.test.csv") as csvfile:
 
     for i, row in enumerate(reader): # each row is a list
 
-        if i!=0:
+        #if i == 0:
+            #headers = row
+        if ' ?' not in row:
             testData.append(row)
+
 
 print('\n')
 print('Testing Data : ')
@@ -65,15 +69,16 @@ avgList = ['workclass', 'education', 'marital-status', 'occupation', 'relationsh
 avgs =  {}
 
 # creates dictionary, each containing the header as a key with the value as an array of possible characteristics
+
 for i, item in enumerate(trainData.T):
 
     if headers[i] in avgList:
         avgs[headers[i]] = []
-
         for info in item:
 
             if info not in avgs[headers[i]]:
-                avgs[headers[i]].append(info)
+                if '?' not in info and info not in headers[i]:
+                    avgs[headers[i]].append(info)
 
 probs = {}
 
@@ -81,14 +86,17 @@ probs = {}
 # these inner dictionaries contain possible headers as keys and probabilities of making 50k or more as values
 #for i, key in enumerate(avgs.keys()):
 for item in trainData.T:
-
+    
     if item[0] in avgs.keys():
-
         probs[item[0]] = []
 
-        for key in avgs.keys()[avgs.keys()==item[0]]:
-            probs[item[0]].append(probability50k(trainData, item[0], key, len(trainData.T)))
+        for key in avgs[item[0]]:
+            probs[item[0]].append(probability50k(trainData, item[0], key))
 
     else:
         probs[item[0]] = []
-        probs[item[0]].append(getLikelihood(trainData, item[0], len(trainData)))
+        probs[item[0]].append(getLikelihood(trainData, item[0]))
+
+
+print()
+print(probs)
